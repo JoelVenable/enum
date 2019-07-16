eed4ec4bd15e911ea75d25eaf9e85cd5e409ab4e
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-15 20:46:49 -0500

    Added named arguments to function

--- Program.cs
+++ Program.cs
@@ -16,11 +16,12 @@ namespace enum_tutorial
       int num2 = 20;
 
       System.Console.WriteLine("Before Swap num1: {0}, num2 {1}", num1, num2);
-        Swap(ref num1, ref num2);
+      Swap(ref num1, ref num2);
       System.Console.WriteLine("After Swap num1: {0}, num2 {1}", num1, num2);
 
-    System.Console.WriteLine("1, 2, 3 = {0}", GetSumMore(1,2,3));
-    
+      System.Console.WriteLine("1, 2, 3 = {0}", GetSumMore(1, 2, 3));
+
+      PrintInfo(zipCode: 15137, name: "Joel");
     }
 
     static void DoubleIt(int x, out int solution)
@@ -30,19 +31,27 @@ namespace enum_tutorial
 
     }
 
-    public static void Swap(ref int num1, ref int num2) {
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
+    public static void Swap(ref int num1, ref int num2)
+    {
+      int temp = num1;
+      num1 = num2;
+      num2 = temp;
     }
 
-    static double GetSumMore(params double[] nums) {
-        double sum = 0;
+    static double GetSumMore(params double[] nums)
+    {
+      double sum = 0;
 
-        foreach (int i in nums) {
-            sum += i;
-        }
-        return sum;
+      foreach (int i in nums)
+      {
+        sum += i;
+      }
+      return sum;
+    }
+
+    static void PrintInfo(string name, int zipCode)
+    {
+      System.Console.WriteLine($"{name} lives in {zipCode}");
     }
   }
 }
